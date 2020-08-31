85dfaee5c85083555ec34bb74c729d8d848c27f8
Valeriy <Valeriy>
2020-08-31 23:13:00 +0700

    Add description

--- FocusTimer_MVVM/Model/Task.swift
+++ FocusTimer_MVVM/Model/Task.swift
@@ -2,7 +2,7 @@
 //  Task.swift
 //  FocusTimer_MVVM
 //
-//  Created by Valeriy Pokatilo on 30.08.2020.
+//  Created by Valeriy Pokatilo on 31.08.2020.
 //  Copyright © 2020 Valeriy Pokatilo. All rights reserved.
 //
 
